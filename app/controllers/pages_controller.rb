4bbe95cfa49c5a5f1eefeb1284460c46a8120f63
Mike Hall <mike@just3ws.com>
2014-05-19 16:44:09 -0500

    Fixed pages controller using :symbol values at runtime and wacky namespacing of initializer constants

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
 
   def show
     show_pages_params = params.permit(:page, :layout)
+
     page_to_show = whitelist_page(show_pages_params[:page])
 
     render action: page_to_show, layout: whitelist_layout(show_pages_params[:layout])
@@ -12,7 +13,7 @@ class PagesController < ApplicationController
 
   # Checks whether the requested_page exists in app/views/pages/*.html.haml
   def whitelist_page(requested_page)
-    raise "Invalid page: #{requested_page}" unless STATIC_PAGES.include?(requested_page)
+    raise "Invalid page: #{requested_page}" unless ::STATIC_PAGES.include?(requested_page.to_s)
 
     requested_page
   end
@@ -20,7 +21,7 @@ class PagesController < ApplicationController
   def whitelist_layout(requested_layout)
     return 'application' if requested_layout.nil?
 
-    raise "Invalid layout: #{requested_layout}" unless STATIC_PAGE_LAYOUTS.include?(requested_layout)
+    raise "Invalid layout: #{requested_layout}" unless ::STATIC_PAGE_LAYOUTS.include?(requested_layout.to_s)
 
     requested_layout
   end
