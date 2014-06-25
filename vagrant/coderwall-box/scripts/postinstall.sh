b2c2260e9c93ba81aa79d1f371cba08388e3293b
Mike Hall <mike@just3ws.com>
2014-06-25 13:09:25 -0500

    Run a cleanup script after building the base box

--- vagrant/coderwall-box/scripts/postinstall.sh
+++ vagrant/coderwall-box/scripts/postinstall.sh
@@ -102,6 +102,9 @@ make install-world
 cd ..
 rm -rf postgresql-$POSTGRES_VERSION*
 
+# Add 'vagrant' role
+su -c 'createuser vagrant -s' postgres
+
 # Initialize postgres DB
 useradd -p postgres postgres
 mkdir -p /var/pgsql/data
@@ -133,11 +136,12 @@ make install
 cd utils
 yes | sudo ./install_server.sh
 cd ../..
-rm -rf redis-$REDIS_VERSION
+rm -rf ~/redis-$REDIS_VERSION
 
 ES_VERSION="0.20.5"
 wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-$ES_VERSION.deb
 dpkg -i elasticsearch-$ES_VERSION.deb
+rm -rf ~/elasticsearch-$ES_VERSION.deb
 
 # Memcached
 apt-get -y install memcached
@@ -190,8 +194,55 @@ apt-get -y autoremove
 # Set locale
 echo 'LC_ALL="en_US.UTF-8"' >> /etc/default/locale
 
-# Add 'vagrant' role
-su -c 'createuser vagrant -s' postgres
+echo "==> Installed packages before cleanup"
+dpkg --get-selections | grep -v deinstall
+
+# Remove some packages to get a minimal install
+echo "==> Removing all linux kernels except the currrent one"
+dpkg --list | awk '{ print $2 }' | grep 'linux-image-3.*-generic' | grep -v $(uname -r) | xargs apt-get -y purge
+echo "==> Removing linux source"
+dpkg --list | awk '{ print $2 }' | grep linux-source | xargs apt-get -y purge
+echo "==> Removing development packages"
+dpkg --list | awk '{ print $2 }' | grep -- '-dev$' | xargs apt-get -y purge
+echo "==> Removing documentation"
+dpkg --list | awk '{ print $2 }' | grep -- '-doc$' | xargs apt-get -y purge
+echo "==> Removing development tools"
+#dpkg --list | grep -i compiler | awk '{ print $2 }' | xargs apt-get -y purge
+#apt-get -y purge cpp gcc g++
+apt-get -y purge build-essential
+echo "==> Removing default system Ruby"
+apt-get -y purge ruby ri doc
+echo "==> Removing default system Python"
+apt-get -y purge python-dbus libnl1 python-smartpm python-twisted-core libiw30 python-twisted-bin libdbus-glib-1-2 python-pexpect python-pycurl python-serial python-gobject python-pam python-openssl libffi5
+echo "==> Removing X11 libraries"
+apt-get -y purge libx11-data xauth libxmuu1 libxcb1 libx11-6 libxext6
+echo "==> Removing obsolete networking components"
+apt-get -y purge ppp pppconfig pppoeconf
+echo "==> Removing other oddities"
+apt-get -y purge popularity-contest installation-report landscape-common wireless-tools wpasupplicant ubuntu-serverguide
+
+# Clean up the apt cache
+apt-get -y autoremove --purge
+apt-get -y autoclean
+apt-get -y clean
+
+# Clean up orphaned packages with deborphan
+apt-get -y install deborphan
+while [ -n "$(deborphan --guess-all --libdevel)" ]; do
+    deborphan --guess-all --libdevel | xargs apt-get -y purge
+done
+apt-get -y purge deborphan dialog
+
+echo "==> Removing man pages"
+rm -rf /usr/share/man/*
+echo "==> Removing APT files"
+find /var/lib/apt -type f | xargs rm -f
+echo "==> Removing anything in /usr/src"
+rm -rf /usr/src/*
+echo "==> Removing any docs"
+rm -rf /usr/share/doc/*
+echo "==> Removing caches"
+find /var/cache -type f -exec rm -rf {} \;
 
 echo "Adding a 2 sec delay to the interface up, to make the dhclient happy"
 echo "pre-up sleep 2" >> /etc/network/interfaces
