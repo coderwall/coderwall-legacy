c60cf04ffdde5966d527b807e3719277f71b5c07
Mike Hall <mike@just3ws.com>
2014-06-25 18:32:07 -0500

    Removed dead prescriptions directory

--- vagrant/prescriptions/elasticsearch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ES_VERSION="0.90.9"
-
-# ElasticSearch
-wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-$ES_VERSION.deb
-dpkg -i elasticsearch-$ES_VERSION.deb
