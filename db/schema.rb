0331d0cc1c8e50af219d45280215dd9473965ac1
Mike Hall <mike@just3ws.com>
2014-07-01 12:02:10 -0500

    Enable pg_stat_statements

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140629031154) do
+ActiveRecord::Schema.define(:version => 20140701170008) do
 
   create_table "alias_tags", :id => false, :force => true do |t|
     t.integer "tag_id"
