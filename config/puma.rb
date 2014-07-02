4f04f5269efa688096d65ecbde06a8aaeda1f4dc
Mike Hall <mike@just3ws.com>
2014-07-02 12:49:49 -0500

    [WIP#145] Tuning Puma with Evan Phoenix

--- config/puma.rb
+++ config/puma.rb
@@ -1,31 +1,23 @@
+# Current Configuration
+# =====================
+# MAX_THREADS:  32
+# MIN_THREADS:  8
+# PUMA_WORKERS: 4
+#
+# half the unicorn config
+#
+# workers 4
+# threads 2
+#
+# let Heroku go and do it's timeouts
+#
+# bringing up a legacy app try starting with workers running 1 thread until confident about thread-safety
+# is threadsafe even on? <= Rails 4
+
 port ENV['PORT'] || '3000'
 
 workers Integer(ENV['PUMA_WORKERS'] || 3)
 threads Integer(ENV['MIN_THREADS']  || 1), Integer(ENV['MAX_THREADS'] || 16)
 
-preload_app!
-
 rackup DefaultRackup
 environment ENV['RACK_ENV'] || 'development'
-
-on_worker_boot do
-  ActiveSupport.on_load(:active_record) do
-    config = ActiveRecord::Base.configurations[Rails.env] || Rails.application.config.database_configuration[Rails.env]
-    config['pool'] = ENV['MAX_THREADS'] || 16
-    ActiveRecord::Base.establish_connection(config)
-  end
-
-  if defined?(Resque)
-    Resque.redis = ENV['REDIS_URL']
-  end
-end
-
-on_restart do
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.connection.disconnect!
-
-  if defined?(Resque)
-    Resque.redis.quit
-    Rails.logger.info('Disconnected from Redis')
-  end
-end
