be72575dbdd27a034d6c1f2c3b78d11e5b769339
Mike Hall <mike@just3ws.com>
2014-07-02 20:37:59 -0500

    Start New Relic monitor without preload_app

--- config/initializers/new_relic.rb
+++ config/initializers/new_relic.rb
@@ -1,4 +1,4 @@
-# Ensure the agent is started using Unicorn
-# This is needed when using Unicorn and preload_app is not set to true.
-# See http://support.newrelic.com/kb/troubleshooting/unicorn-no-data
-#NewRelic::Agent.after_fork(:force_reconnect => true) if defined? Unicorn
+if Rails.env.production?
+  ::NewRelic::Agent.manual_start()
+  ::NewRelic::Agent.after_fork(force_reconnect: true)
+end
