ec2af7fc85bf2ad0a484b2529c52f6ce999d4b6d
Mike Hall <mike@just3ws.com>
2014-07-03 21:03:11 -0500

    Added indexes to allow for User.with_username queries to avoid seq scans

---
+++ db/migrate/20140703223632_create_case_insensitive_indexes_on_user.rb
@@ -0,0 +1,29 @@
+class CreateCaseInsensitiveIndexesOnUser < ActiveRecord::Migration
+
+  # User.with_username looks up on following fields almost
+  # constantly but with a UPPER(fieldname) = UPPER(val)
+  # which is nasty and slow, add upcase and downcase indexes
+  # to avoid the problem
+
+  def up
+    execute 'create index ix_users_github_lower on users (lower(github) varchar_pattern_ops)'
+    execute 'create index ix_users_github_upper on users (upper(github) varchar_pattern_ops)'
+    execute 'create index ix_users_linkedin_lower on users (lower(linkedin) varchar_pattern_ops)'
+    execute 'create index ix_users_linkedin_upper on users (upper(linkedin) varchar_pattern_ops)'
+    execute 'create index ix_users_twitter_lower on users (lower(twitter) varchar_pattern_ops)'
+    execute 'create index ix_users_twitter_upper on users (upper(twitter) varchar_pattern_ops)'
+    execute 'create index ix_users_username_lower on users (lower(username) varchar_pattern_ops)'
+    execute 'create index ix_users_username_upper on users (upper(username) varchar_pattern_ops)'
+  end
+
+  def down
+    execute 'drop index if exists ix_users_github_lower'
+    execute 'drop index if exists ix_users_github_upper'
+    execute 'drop index if exists ix_users_linkedin_lower'
+    execute 'drop index if exists ix_users_linkedin_upper'
+    execute 'drop index if exists ix_users_twitter_lower'
+    execute 'drop index if exists ix_users_twitter_upper'
+    execute 'drop index if exists ix_users_username_lower'
+    execute 'drop index if exists ix_users_username_upper'
+  end
+end
