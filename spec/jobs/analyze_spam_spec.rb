1cfb2522d074cd970b487882f716a96e2edf8f0a
Mike Hall <mike@just3ws.com>
2014-07-08 17:59:28 -0500

    Fixed broken tests for Akismet spam analysis

--- spec/jobs/analyze_spam_spec.rb
+++ spec/jobs/analyze_spam_spec.rb
@@ -1,22 +1,21 @@
 describe AnalyzeSpam do
-  let(:spammable){ Fabricate(:comment) }
-  before { @analyzer = AnalyzeSpam.new(spammable) }
-
-  describe "#perform" do
-    context "when it's a spam" do
-      before { spammable.stub(:spam?).and_return true }
-
-      it "should create a spam report" do
-        @analyzer.perform
+  describe '#perform' do
+    context 'when it is a spam' do
+      it 'should create a spam report' do
+        Protip.any_instance.stub(:index_search)
+        spammable = Fabricate(:comment)
+        spammable.stub(:spam?).and_return(true)
+        AnalyzeSpam.new(spammable).perform
         spammable.spam_report.should_not be_nil
       end
     end
 
-    context "when it's not a spam" do
-      before { spammable.stub(:spam?).and_return false }
+    context 'when it is not a spam' do
 
-      it "should not create a spam report" do
-        @analyzer.perform
+      it 'should not create a spam report' do
+        Protip.any_instance.stub(:index_search)
+        spammable = Fabricate(:comment)
+        spammable.stub(:spam?).and_return(false)
         spammable.spam_report.should be_nil
       end
     end
