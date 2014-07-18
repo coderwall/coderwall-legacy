8928ea2c9632096ac0d9209cc2b46a78b86c815b
Mike Hall <mike@just3ws.com>
2014-07-18 16:27:27 -0500

    Set Sidekiq configuration per Heroku's policies

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,8 @@
+# https://devcenter.heroku.com/articles/forked-pg-connections#sidekiq
+Sidekiq.configure_server do |config|
+  database_url = ENV['DATABASE_URL']
+  if database_url
+    ENV['DATABASE_URL'] = "#{database_url}?pool=25"
+    ActiveRecord::Base.establish_connection
+  end
+end
