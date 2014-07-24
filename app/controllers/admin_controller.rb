6a2e63c6d5df3f9379c39dd0857338850403d47b
Abdelkader Boudih <terminale@gmail.com>
2014-07-24 12:26:43 +0000

    Clean up admin controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,38 +3,7 @@ class AdminController < BaseAdminController
   def index
   end
 
-  def failed_jobs
-    @page     = params[:page].try(:to_i) || 1
-    @per_page = params[:per_page].try(:to_i) || 10
-
-    @total_failed = Delayed::Job.
-      where("last_error IS NOT NULL").
-      count
-
-    @jobs = Delayed::Job.
-      where("last_error IS NOT NULL").
-      offset((@page - 1) * @per_page).
-      limit(@per_page).
-      order("updated_at DESC")
-  end
-
-  if Rails.env.development?
-    skip_before_action :require_admin!, only: [:index, :toggle_premium_team]
-
-    def toggle_premium_team
-      team         = current_user.team || begin
-        team = Team.first
-        team.add_user(current_user)
-      end
-      team.premium = !team.premium
-      team.save!
-      return redirect_to('/')
-    end
-
-  end
-
   def teams
-
   end
 
   def sections_teams
@@ -46,7 +15,6 @@ class AdminController < BaseAdminController
   end
 
   def parse_section_name(section_name)
-    name = Team::SECTIONS.select { |section| section == section_name }.first
-    return name.to_sym unless name.nil?
+    section_name.to_sym if Team::SECTIONS.include? section_name
   end
 end
