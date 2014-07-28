edb52c4d7f53849ec5c69caa24a1f72115b945bf
Abdelkader Boudih <terminale@gmail.com>
2014-07-28 08:45:33 +0000

    [Fix] Skip analyse spam in dev

--- app/jobs/analyze_spam_job.rb
+++ app/jobs/analyze_spam_job.rb
@@ -4,7 +4,7 @@ class AnalyzeSpamJob
   sidekiq_options queue: :medium
 
   def perform(spammable)
-    return if Rails.env.test?
+    return if Rails.env.test? || Rails.env.development?
     thing_to_analyze = spammable['klass'].classify.constantize.find(spammable['id'])
 
     if thing_to_analyze.spam?
