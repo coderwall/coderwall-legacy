ca630f747dc844d50e97fb6e4a89037931c4d85b
Mike Hall <mike@just3ws.com>
2014-07-31 14:45:55 -0500

    Fixed the duplicate usernames in the system and now can apply the unique index on users/usernames

--- db/migrate/20140729091832_enable_citext_for_user_user_name.rb
+++ db/migrate/20140729091832_enable_citext_for_user_user_name.rb
@@ -1,7 +1,7 @@
 class EnableCitextForUserUserName < ActiveRecord::Migration
   def up
     execute 'CREATE EXTENSION IF NOT EXISTS "citext"'
-    change_column :users, :username, :citext, index:true # , unique: true
-    change_column :users, :email, :citext, index:true #, unique: true
+    change_column :users, :username, :citext, index:true, unique: true
+    change_column :users, :email, :citext, index:true, unique: true
   end
 end
