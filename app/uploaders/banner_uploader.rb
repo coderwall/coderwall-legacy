e7be4191a337bdf0ece69da054a86c77901bce64
Mike Hall <mike@just3ws.com>
2014-08-02 18:13:24 -0500

    Fixed tiltshift banner error because we were moving a file like  instead of just

--- app/uploaders/banner_uploader.rb
+++ app/uploaders/banner_uploader.rb
@@ -1,15 +1,9 @@
+require 'fileutils'
 class BannerUploader < CoderwallUploader
-
-  #process :apply_tilt_shift
-  # process :resize_to_fill => [500, 375]
-  #process :resize_to_fit => [500, 375]
-
   def apply_tilt_shift
     directory = File.dirname(current_path)
     tmpfile = File.join(directory, "tmpfile")
-    #record_event('uploading bg image')
-    #Resque.enqueue(ProcessImage, :background_image, )
-    File.send(:move, current_path, tmpfile)
+    FileUtils.mv(current_path, tmpfile)
     system "convert #{tmpfile} -sigmoidal-contrast 7x50% \\( +clone -sparse-color Barycentric '0,0 black 0,%h white' -function polynomial 4.5,-4.5,1 \\) -compose Blur -set option:compose:args 15 -composite #{current_path}"
     File.delete(tmpfile)
   end
