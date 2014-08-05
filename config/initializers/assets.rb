a02e0054a4f1146a127105e664dc2843cdeb3b60
Mike Hall <mike@just3ws.com>
2014-08-05 14:04:09 -0500

    Restored ember to the config/initializers/assets.rb manifest

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,29 +1,38 @@
 Coderwall::Application.configure do
   config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
-  config.assets.precompile << %w(
-    account.js
-    accounts.js
-    admin.css
-    application.css
-    application.js
-    autosaver.js
-    connections.js
-    ember/dashboard.js
-    ember/teams.js
-    premium-admin.js
-    premium-teams.css
-    premium.js
-    product_description.css
-    protip.css
-    protips-grid.js
-    protips.js
-    search.js
-    settings.js
-    teams.js
-    tracking.js
-    user.js
-    username-validation.js
-  )
-
+  config.assets.precompile << 'admin.css'
+  config.assets.precompile << 'application.css'
+  config.assets.precompile << 'application.js'
+  config.assets.precompile << 'product_description.css'
+  config.assets.precompile << 'premium-teams.css'
+  config.assets.precompile << 'protip.css'
+  config.assets.precompile << 'account.js'
+  config.assets.precompile << 'protips.js'
+  config.assets.precompile << 'ember/dashboard.js'
+  config.assets.precompile << 'connections.js'
+  config.assets.precompile << 'jquery.js'
+  config.assets.precompile << 'jquery_ujs.js'
+  config.assets.precompile << 'hyphernator/hyphernator.js'
+  config.assets.precompile << 'search.js'
+  config.assets.precompile << 'history.adapter.jquery.js'
+  config.assets.precompile << 'history.js'
+  config.assets.precompile << 'protips-grid.js'
+  config.assets.precompile << 'underscore.js'
+  config.assets.precompile << 'html5shiv.js'
+  config.assets.precompile << 'tracking.js'
+  config.assets.precompile << 'teams.js'
+  config.assets.precompile << 'ember/teams.js'
+  config.assets.precompile << 'jquery.scrolldepth.js'
+  config.assets.precompile << 'premium.js'
+  config.assets.precompile << 'premium-admin.js'
+  config.assets.precompile << 'accounts.js'
+  config.assets.precompile << 'jquery.effects.core.js'
+  config.assets.precompile << 'jquery.effects.slide.js'
+  config.assets.precompile << 'settings.js'
+  config.assets.precompile << 'username-validation.js'
+  # config.assets.precompile << 'jquery-ketchup.all.min.js'
+  config.assets.precompile << 'user.js'
+  config.assets.precompile << 'autosaver.js'
   config.assets.version = '1.1'
 end
+
