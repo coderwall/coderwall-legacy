056d59ab783287318e886e4c9627dffb0ca1dc30
Abdelkader Boudih <terminale@gmail.com>
2014-08-09 08:45:39 +0000

    Fix layouts Add initial controller specs change top menu icon color to white

---
+++ spec/controllers/opportunity_controlller_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+RSpec.describe OpportunitiesController, :type => :controller do
+
+  it 'render #index' do
+    get :index
+    expect(response.status).to eq(200)
+    expect(response).to render_template(['opportunities/index', 'layouts/jobs'])
+  end
+
+end
