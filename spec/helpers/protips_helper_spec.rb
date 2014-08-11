b56a5bbf5bda31f24d29f256c1c3c74c7e194bb7
Mike Hall <mike@just3ws.com>
2014-08-11 14:58:06 -0500

    Moved some convoluted logic to determine which protip search results into a helper and created a placeholder spec

--- spec/helpers/protips_helper_spec.rb
+++ spec/helpers/protips_helper_spec.rb
@@ -1,6 +1,7 @@
-require 'spec_helper'
-
-RSpec.describe ProtipsHelper, :type => :helper do
-
-
+RSpec.describe ProtipsHelper, type: :helper do
+  describe ".protip_search_results_to_render" do
+    it 'has no protips to render' do
+      expect(helper.protip_search_results_to_render(nil)).to be_nil
+    end
+  end
 end
