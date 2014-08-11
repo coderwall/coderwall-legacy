873dd7c5e59d59392bfcb7a7d10dbc217b5f5b65
Mike Hall <mike@just3ws.com>
2014-08-11 15:43:21 -0500

    Fixed a bug where the job would be enqueued ~58 hours in the future

--- app/workers/activate_pending_users_worker.rb
+++ app/workers/activate_pending_users_worker.rb
@@ -9,10 +9,8 @@ class ActivatePendingUsersWorker
     # but would like to spread their execution
     # over the next hour to avoid overloading
     # the database.
-    delay = 0
     User.pending.find_each(batch_size: 100) do |user|
-      UserActivateWorker.perform_in(delay.minutes, user.id)
-      delay = delay + Random.rand(0..59)
+      UserActivateWorker.perform_in(Random.rand(0..59).minutes, user.id)
     end
   end
 end
