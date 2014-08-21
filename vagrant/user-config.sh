33d8188d9c307c598ad64d6af1132282bb99aef8
Mike Hall <mike@just3ws.com>
2014-08-21 11:58:14 -0500

    Fixed typo in RVM flag

--- vagrant/user-config.sh
+++ vagrant/user-config.sh
@@ -4,11 +4,14 @@ cd /home/vagrant
 echo Who am I? You are `whoami`.
 echo Where am I? You are in `pwd`
 echo I think my home is $HOME
+
 echo export EDITOR=vim >> $HOME/.bashrc
+
 echo insecure > $HOME/.curlrc
+
 echo rvm_install_on_use_flag=1 >> $HOME/.rvmrc
 echo rvm_project_rvmrc=1 >> $HOME/.rvmrc
-echo rvm_trust_rvmrcs_fag=1 >> $HOME/.rvmrc
+echo rvm_trust_rvmrcs_flag=1 >> $HOME/.rvmrc
 curl -k -L https://get.rvm.io | bash -s stable --autolibs=enabled
 source "$HOME/.rvm/scripts/rvm"
 [[ -s "$rvm_path/hooks/after_cd_bundle" ]] && chmod +x $rvm_path/hooks/after_cd_bundle
