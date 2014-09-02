5bcfdfee9d253d07ee99907945bdf023989a44a2
Mike Hall <mike@just3ws.com>
2014-09-02 16:20:46 -0500

    Stacked the schedule for the jobs

--- app/clock.rb
+++ app/clock.rb
@@ -5,15 +5,6 @@ require_relative '../config/environment'
 
 include Clockwork
 
-# Runs as 1:01 AM Pacific
-every(1.day, 'award:activate:active', at: '01:01') do
-  ActivatePendingUsersWorker.perform_async
-end
-
-every(1.day, 'award:refresh:stale', at: '00:00') do
-  RefreshStaleUsersWorker.perform_async
-end
-
 # On the first of every month send the popular protips from the previous month.
 every(1.day, 'protip_mailer:popular_protips', if: ->(t){ t.day == 1 }) do
   if ENV['PROTIP_MAILER_POPULAR_PROTIPS']
@@ -24,25 +15,34 @@ every(1.day, 'protip_mailer:popular_protips', if: ->(t){ t.day == 1 }) do
   end
 end
 
-every(1.day, 'cleanup:protips:associate_zombie_upvotes', at: '03:30') do
-  CleanupProtipsAssociateZombieUpvotesJob.perform_async
-end
-
-every(1.day, 'clear_expired_sessions', at: '06:00') do
-  ClearExpiredSessionsJob.perform_async
-end
-
-every(1.day, 'protips:recalculate_scores', at: '03:00') do
-  ProtipsRecalculateScoresJob.perform_async
-end
-
-every(1.day, 'search:sync', at: '04:00') do
-  SearchSyncJob.perform_async
-end
-
-every(1.day, 'teams:refresh', at: '05:00') do
+every(1.day, 'teams:refresh', at: '22:00') do
   TeamsRefreshJob.perform_async
 end
 
+every(1.day, 'award:refresh:stale', at: '00:00') do
+  RefreshStaleUsersWorker.perform_async
+end
+
+# Runs as 1:00 AM Pacific
+every(1.day, 'award:activate:active', at: '01:00') do
+  ActivatePendingUsersWorker.perform_async
+end
+
+every(1.day, 'cleanup:protips:associate_zombie_upvotes', at: '02:00') do
+  CleanupProtipsAssociateZombieUpvotesJob.perform_async
+end
+
+every(1.day, 'search:sync', at: '03:00') do
+  SearchSyncJob.perform_async
+end
+
+every(1.day, 'protips:recalculate_scores', at: '04:00') do
+  ProtipsRecalculateScoresJob.perform_async
+end
+
+every(1.day, 'clear_expired_sessions', at: '05:00') do
+  ClearExpiredSessionsJob.perform_async
+end
+
 # This is tied with broken code. Probably should delete
-every(1.day, 'facts:system', at: '00:00') {}
+# every(1.day, 'facts:system', at: '00:00') {}
