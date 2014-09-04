92bc485cb03438a0a99d3b82585b7776ebd65b40
Mike Hall <mike@just3ws.com>
2014-09-04 07:21:38 -0500

    Added more paranoia to the sidekiq worker because some emails are invalid

--- app/workers/protip_mailer_popular_protips_worker.rb
+++ app/workers/protip_mailer_popular_protips_worker.rb
@@ -6,7 +6,17 @@ class ProtipMailerPopularProtipsWorker
     protips = ProtipMailer::Queries.popular_protips(from, to)
 
     User.find_each(batch_size: 100) do |user|
-      ProtipMailer.popular_protips(user, protips, from, to).deliver
+      begin
+        if REDIS.sismember(ProtipMailer::CAMPAIGN_ID, user.id.to_s)
+          Rails.logger.warn("Already sent email to #{user.id} please check Redis SET #{ProtipMailer::CAMPAIGN_ID}.")
+        else
+          Rails.logger.warn("Sending email to #{user.id}.")
+          ProtipMailer.popular_protips(user, protips, from, to).deliver
+        end
+      rescue => ex
+        Rails.logger.error("[ProtipMailer.popular_protips] Unable to send email due to '#{ex}' >>\n#{ex.backtrace.join("\n  ")}")
+        Rails.logger.ap([from, to, user, protips], :error)
+      end
     end
   end
 end
