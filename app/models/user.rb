06fc1da65672541b05bc8befdf46ec734c9344ce
Mike Hall <mike@just3ws.com>
2014-09-10 18:35:52 -0500

    Log a dropped team connection as a fatal error

--- app/models/user.rb
+++ app/models/user.rb
@@ -217,7 +217,8 @@ class User < ActiveRecord::Base
 
   def team
     @team ||= team_document_id && Team.find(team_document_id)
-  rescue Mongoid::Errors::DocumentNotFound
+  rescue Mongoid::Errors::DocumentNotFound => ex
+    Rails.logger.fatal("[TEAMS] Couldn't find Team using '#{team_document_id}' for user: '#{id}' -- '#{ex}' >>\n#{ex.backtrace.join("\n  ")}")
     #readonly issue in follows/_user partial from partial iterator
     User.connection.execute("UPDATE users set team_document_id = NULL where id = #{self.id}")
     @team = nil
@@ -616,18 +617,18 @@ class User < ActiveRecord::Base
   end
 
   #def build_repo_followed_activity!(refresh=false)
-    #Redis.current.zremrangebyrank(followed_repo_key, 0, Time.now.to_i) if refresh
-    #epoch_now  = Time.now.to_i
-    #first_time = refresh || Redis.current.zcount(followed_repo_key, 0, epoch_now) <= 0
-    #links      = GithubOld.new.activities_for(self.github, (first_time ? 20 : 1))
-    #links.each do |link|
-      #link[:user_id] = self.id
-      #Redis.current.zadd(followed_repo_key, link[:date].to_i, link.to_json)
-      #Importers::Protips::GithubImporter.import_from_follows(link[:description], link[:link], link[:date], self)
-    #end
+  #Redis.current.zremrangebyrank(followed_repo_key, 0, Time.now.to_i) if refresh
+  #epoch_now  = Time.now.to_i
+  #first_time = refresh || Redis.current.zcount(followed_repo_key, 0, epoch_now) <= 0
+  #links      = GithubOld.new.activities_for(self.github, (first_time ? 20 : 1))
+  #links.each do |link|
+  #link[:user_id] = self.id
+  #Redis.current.zadd(followed_repo_key, link[:date].to_i, link.to_json)
+  #Importers::Protips::GithubImporter.import_from_follows(link[:description], link[:link], link[:date], self)
+  #end
   #rescue RestClient::ResourceNotFound
-    #Rails.logger.warn("Unable to get activity for github #{github}")   if ENV['DEBUG']
-    #[]
+  #Rails.logger.warn("Unable to get activity for github #{github}")   if ENV['DEBUG']
+  #[]
   #end
 
   def destroy_github_cache
