dd6708dc6d736c20146f47289ae97184e7af0a2a
Mike Hall <mike@just3ws.com>
2014-09-25 12:32:53 -0500

    Added helpers to make starting the app in one command easier

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -6,7 +6,8 @@ su -c '/usr/bin/pg_ctl start -l /var/pgsql/data/log/logfile -D /var/pgsql/data' 
 
 sudo su - vagrant <<-'EOF'
   cd ~/web
-  bundle check && bundle install
+  bundle check || bundle install
+  # Force the app to use the internal Postgres port number and ignore .env
   DEV_POSTGRES_PORT=5432 bundle exec rake db:migrate
   DEV_POSTGRES_PORT=5432 bundle exec rake db:test:prepare
 EOF
