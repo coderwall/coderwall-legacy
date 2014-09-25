9803ed6dcf4e3342ff3569f7ad7249bd89281316
Mike Hall <mike@just3ws.com>
2014-09-25 00:39:15 -0500

    Updated the Vagrant bootstrap to be entirely self-contained. The new box image is ready to run from the word 'up'.

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,17 +1,12 @@
 #!/bin/bash -x
 export DEBIAN_FRONTEND=noninteractive
-cd /home/vagrant
-echo Who am I? You are `whoami`.
-echo Where am I? You are in `pwd`
-echo I think my home is $HOME
-echo export EDITOR=vim >> $HOME/.bashrc
-# Enable accessing Postgres from the host machine
-apt-get -y install libcurl3 libcurl3-dev libcurl3-gnutls libcurl4-openssl-dev
-apt-get -y install libpq-dev
-apt-get -y install libxml2 libxml2-dev libxslt1-dev
-echo "listen_addresses = '*'" | tee -a /var/pgsql/data/postgresql.conf
-echo host all all  0.0.0.0/0 trust | tee -a  /var/pgsql/data/pg_hba.conf
-sudo su postgres -c 'pg_ctl stop -D /var/pgsql/data  2>&1'
-sudo su postgres -c 'pg_ctl start -D /var/pgsql/data  2>&1 &'
-su -c "ln -s /vagrant /home/vagrant/web" vagrant
-su -c "source /home/vagrant/web/vagrant/user-config.sh" vagrant
+
+# Ensure the database is started
+su -c '/usr/bin/pg_ctl start -l /var/pgsql/data/log/logfile -D /var/pgsql/data' postgres
+
+sudo su - vagrant <<-'EOF'
+  cd ~/web
+  bundle check && bundle install
+  DEV_POSTGRES_PORT=5432 bundle exec rake db:migrate
+  DEV_POSTGRES_PORT=5432 bundle exec rake db:test:prepare
+EOF
