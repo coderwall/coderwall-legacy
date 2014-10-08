5646f0d311495a9d8be1689eef917c8d85742c61
Mike Hall <mike@just3ws.com>
2014-10-08 11:54:01 -0500

    Explode if Airbrake is not installed

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,9 +1,3 @@
-if defined?(Airbrake)
-  Airbrake.configure do |config|
-    config.api_key = ENV['AIRBRAKE_API_KEY']
-  end
-else
-  unless Rails.env.test? || Rails.env.development?
-    Rails.logger.warn '[WTF WARNING] Someone deleted airbrake and forgot the initializer'
-  end
+Airbrake.configure do |config|
+  config.api_key = ENV['AIRBRAKE_API_KEY']
 end
