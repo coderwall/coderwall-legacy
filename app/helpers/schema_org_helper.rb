39168b4f1355e606a98848a0088ab50f793eb402
Mike Hall <mike@just3ws.com>
2014-10-24 11:59:47 -0500

    Use HTTPS for schema.org requests

--- app/helpers/schema_org_helper.rb
+++ app/helpers/schema_org_helper.rb
@@ -1,17 +1,17 @@
 module SchemaOrgHelper
   def meta_person_schema_url
-    'http://schema.org/Person'
+    'https://schema.org/Person'
   end
 
   def meta_address_schema_url
-    'http://schema.org/Address'
+    'https://schema.org/Address'
   end
 
   def meta_article_schema_url
-    'http://schema.org/TechArticle'
+    'https://schema.org/TechArticle'
   end
 
   def meta_comment_schema_url
-    'http://schema.org/Comment'
+    'https://schema.org/Comment'
   end
 end
