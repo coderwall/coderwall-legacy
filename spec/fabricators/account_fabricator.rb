6f019582f49a658bcaf3df6a45a5186c5f75d8bd
Yaro Apletov <yaro@mail.ru>
2014-11-03 15:10:34 +0000

    minor refactoring
    updated fabricators
    
    invoicer rendering refactoring

--- spec/fabricators/account_fabricator.rb
+++ spec/fabricators/account_fabricator.rb
@@ -1,2 +1,6 @@
 Fabricator(:account) do
+  name 'test_account'
+  admin_id 1
+  stripe_card_token { "tok_14u7LDFs0zmMxCeEU3OGRUa0_#{rand(1000)}" }
+  stripe_customer_token { "cus_54FsD2W2VkrKpW_#{rand(1000)}" }
 end
