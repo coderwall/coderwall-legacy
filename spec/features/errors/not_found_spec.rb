fe4e15b515198cb2f3ae169f2157ce2552a40843
Matthew Bender <benderm@gmail.com>
2014-11-22 15:58:18 -0700

    display 404 page: invalid route / record not found
    - rescue_from ActionController::RoutingError doesn’t do anything
    - `unless Rails.env.production?` made record not found work
    - added tests! :-)
    - new errors controller
    - handle 422 & 500 errors with public pages
    - annotate routes

---
+++ spec/features/errors/not_found_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+feature "Custom 404 Page" do
+  before(:all) do
+    Rails.application.config.action_dispatch.show_exceptions = true
+    Rails.application.config.consider_all_requests_local = false
+  end
+
+  after(:all) do
+    Rails.application.config.action_dispatch.show_exceptions = false
+    Rails.application.config.consider_all_requests_local = true
+  end
+
+  scenario 'user is presented 404 page when they visit invalid path' do
+    visit '/fake/path/doesnt/match/route'
+
+    expect(page).to have_content('Uh oh, something went wrong!')
+  end
+
+  scenario 'user is presented 404 page when then visit a bad user path' do
+    visit '/not_a_real_username'
+
+    expect(page).to have_content('Uh oh, something went wrong!')
+  end
+end
