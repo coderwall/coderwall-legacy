18de2d5380b7a6de548a3049c59fe761196aac59
Nícolas Iensen <nicolas.iensen@gmail.com>
2014-12-21 08:48:56 -0200

    Refactor opportunities controller tests to separate one assert per test

--- spec/controllers/opportunity_controlller_spec.rb
+++ spec/controllers/opportunity_controlller_spec.rb
@@ -2,10 +2,15 @@ require 'spec_helper'
 
 RSpec.describe OpportunitiesController, type: :controller do
 
-  it 'render #index' do
-    get :index
-    expect(response.status).to eq(200)
-    expect(response).to render_template(['opportunities/index', 'layouts/jobs'])
-  end
+  describe "GET index" do
+    it "should respond with 200" do
+      get :index
+      expect(response.status).to eq(200)
+    end
 
+    it "should render the opportunities index template with jobs layout" do
+      get :index
+      expect(response).to render_template(['opportunities/index', 'layouts/jobs'])
+    end
+  end
 end
