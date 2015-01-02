4a97ba92c6b5eabdfde3508b7dfbaa0f043a56f6
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-01-02 15:29:27 -0200

    Fix ashcat_spec

--- spec/models/badges/ashcat_spec.rb
+++ spec/models/badges/ashcat_spec.rb
@@ -1,5 +1,12 @@
 require 'vcr_helper'
 
+VCR.configure do |c|
+  c.default_cassette_options = {
+    :match_requests_on => [:method,
+                           VCR.request_matchers.uri_without_param(:client_id, :client_secret)]
+  }
+end
+
 RSpec.describe Ashcat, type: :model do
   let(:profile) { Fabricate(:github_profile) }
   let(:contributor) { Fabricate(:user, github_id: profile.github_id, github: 'dhh') }
