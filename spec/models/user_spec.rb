e847d7763670463bdf65cc417e1b35f00e6c789b
Rohit Paul Kuruvilla <rohitpaulk@live.com>
2015-01-11 19:08:52 +0530

    Fix user model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -114,6 +114,72 @@ RSpec.describe User, type: :model do
     User.destroy_all
   end
 
+  describe 'validation' do
+    it 'should not allow a username in the reserved list' do
+      User::RESERVED.each do |reserved|
+        user = Fabricate.build(:user, username: reserved)
+        expect(user).not_to be_valid
+        expect(user.errors[:username]).to eq(['is reserved'])
+      end
+    end
+
+    it 'should not allow a username with a period character' do
+      user = Fabricate.build(:user, username: 'foo.bar')
+      expect(user).not_to be_valid
+      expect(user.errors[:username]).to eq(['must not contain a period'])
+    end
+
+    it 'should require valid email when instantiating' do
+      user = Fabricate.build(:user, email: 'someting @ not valid.com', state: nil)
+      expect(user.save).to be_falsey
+      expect(user).not_to be_valid
+      expect(user.errors[:email]).not_to be_empty
+    end
+  end
+
+  it 'should create a token on creation' do
+    user = Fabricate(:user)
+    expect(user.referral_token).not_to be_nil
+  end
+
+  it 'should not allow the username in multiple cases to be use on creation' do
+    Fabricate(:user, username: 'MDEITERS')
+    expect { Fabricate(:user, username: 'mdeiters') }.to raise_error('Validation failed: Username has already been taken')
+  end
+
+  it 'should find user by username no matter the case' do
+    user = Fabricate(:user, username: 'seuros')
+    expect(User.find_by_username('Seuros')).to eq(user)
+  end
+
+  it 'should not return incorrect user because of pattern matching' do
+    user = Fabricate(:user, username: 'MDEITERS')
+    found = User.find_by_username('M_EITERS')
+    expect(found).not_to eq(user)
+  end
+
+  describe '::find_by_provider_username' do
+    it 'should find users by username when provider is blank' do
+      user = Fabricate(:user, username: 'mdeiters')
+      expect(User.find_by_provider_username('mdeiters', '')).to eq(user)
+    end
+
+    it 'should find users ignoring case' do
+      user = Fabricate(:user, username: 'MDEITERS', twitter: 'MDEITERS', github: 'MDEITERS', linkedin: 'MDEITERS')
+      expect(User.find_by_provider_username('mdeiters', '')).to eq(user)
+      expect(User.find_by_provider_username('mdeiters', "twitter")).to eq(user)
+      expect(User.find_by_provider_username('mdeiters', "github")).to eq(user)
+      expect(User.find_by_provider_username('mdeiters', "linkedin")).to eq(user)
+    end
+  end
+
+  it 'instantiates new user with omniauth if the user is not on file' do
+    omniauth = { 'info' => { 'name' => 'Matthew Deiters', 'urls' => { 'Blog' => 'http://www.theagiledeveloper.com', 'GitHub' => 'http://github.com/mdeiters' }, 'nickname' => 'mdeiters', 'email' => '' }, 'uid' => 7330, 'credentials' => { 'token' => 'f0f6946eb12c4156a7a567fd73aebe4d3cdde4c8' }, 'extra' => { 'user_hash' => { 'plan' => { 'name' => 'micro', 'collaborators' => 1, 'space' => 614_400, 'private_repos' => 5 }, 'gravatar_id' => 'aacb7c97f7452b3ff11f67151469e3b0', 'company' => nil, 'name' => 'Matthew Deiters', 'created_at' => '2008/04/14 15:53:10 -0700', 'location' => '', 'disk_usage' => 288_049, 'collaborators' => 0, 'public_repo_count' => 18, 'public_gist_count' => 31, 'blog' => 'http://www.theagiledeveloper.com', 'following_count' => 27, 'id' => 7330, 'owned_private_repo_count' => 2, 'private_gist_count' => 2, 'type' => 'User', 'permission' => nil, 'total_private_repo_count' => 2, 'followers_count' => 19, 'login' => 'mdeiters', 'email' => '' } }, 'provider' => 'github' }
+
+    user = User.for_omniauth(omniauth.with_indifferent_access)
+    expect(user).to be_new_record
+  end
+
   describe 'viewing' do
     it 'tracks when a user views a profile' do
       user = Fabricate :user
@@ -129,128 +195,78 @@ RSpec.describe User, type: :model do
       expect(user.total_views).to eq(1)
     end
   end
+  describe 'badges' do
+    it 'should return users with most badges' do
+      user_with_2_badges = Fabricate :user, username: 'somethingelse'
+      user_with_2_badges.badges.create!(badge_class_name: Mongoose3.name)
+      user_with_2_badges.badges.create!(badge_class_name: Octopussy.name)
 
-  it 'should create a token on creation' do
-    user = Fabricate(:user)
-    expect(user.referral_token).not_to be_nil
-  end
+      user_with_3_badges = Fabricate :user
+      user_with_3_badges.badges.create!(badge_class_name: Mongoose3.name)
+      user_with_3_badges.badges.create!(badge_class_name: Octopussy.name)
+      user_with_3_badges.badges.create!(badge_class_name: Mongoose.name)
 
-  it 'should not allow the username in multiple cases to be use on creation' do
-    Fabricate(:user, username: 'MDEITERS')
-    lambda do
-      expect(Fabricate(:user, username: 'mdeiters')).to raise_error('Validation failed: Username has already been taken')
+      expect(User.top(1)).to include(user_with_3_badges)
+      expect(User.top(1)).not_to include(user_with_2_badges)
     end
-  end
 
-  it 'should find user by username no matter the case' do
-    user = Fabricate(:user, username: 'seuros')
-    expect(User.find_by_username('Seuros')).to eq(user)
-  end
+    it 'returns badges in order created with latest first' do
+      user = Fabricate :user
+      badge1 = user.badges.create!(badge_class_name: Mongoose3.name)
+      user.badges.create!(badge_class_name: Octopussy.name)
+      badge3 = user.badges.create!(badge_class_name: Mongoose.name)
 
-  it 'should not return incorrect user because of pattern matching' do
-    user = Fabricate(:user, username: 'MDEITERS')
-    found = User.find_by_username('M_EITERS')
-    expect(found).not_to eq(user)
-  end
-
-  it 'should find users by username when provider is blank' do
-    user = Fabricate(:user, username: 'mdeiters')
-    expect(User.find_by_username('mdeiters', '')).to eq(user)
-    expect(User.find_by_username('mdeiters', nil)).to eq(user)
-  end
-
-  it 'should find users ignoring case' do
-    user = Fabricate(:user, username: 'MDEITERS', twitter: 'MDEITERS', github: 'MDEITERS', linkedin: 'MDEITERS')
-    expect(User.find_by_username('mdeiters')).to eq(user)
-    expect(User.find_by_username('mdeiters', :twitter)).to eq(user)
-    expect(User.find_by_username('mdeiters', :github)).to eq(user)
-    expect(User.find_by_username('mdeiters', :linkedin)).to eq(user)
-  end
-
-  it 'should return users with most badges' do
-    user_with_2_badges = Fabricate :user, username: 'somethingelse'
-    badge1 = user_with_2_badges.badges.create!(badge_class_name: Mongoose3.name)
-    badge2 = user_with_2_badges.badges.create!(badge_class_name: Octopussy.name)
-
-    user_with_3_badges = Fabricate :user
-    badge1 = user_with_3_badges.badges.create!(badge_class_name: Mongoose3.name)
-    badge2 = user_with_3_badges.badges.create!(badge_class_name: Octopussy.name)
-    user_with_3_badges.badges.create!(badge_class_name: Mongoose.name)
-
-    expect(User.top(1)).to include(user_with_3_badges)
-    expect(User.top(1)).not_to include(user_with_2_badges)
-  end
-
-  it 'should require valid email when instantiating' do
-    u = Fabricate.build(:user, email: 'someting @ not valid.com', state: nil)
-    expect(u.save).to be_falsey
-    expect(u).not_to be_valid
-    expect(u.errors[:email]).not_to be_empty
-  end
-
-  it 'returns badges in order created with latest first' do
-    user = Fabricate :user
-    badge1 = user.badges.create!(badge_class_name: Mongoose3.name)
-    user.badges.create!(badge_class_name: Octopussy.name)
-    badge3 = user.badges.create!(badge_class_name: Mongoose.name)
-
-    expect(user.badges.first).to eq(badge3)
-    expect(user.badges.last).to eq(badge1)
-  end
-
-  class NotaBadge < BadgeBase
-  end
-
-  class AlsoNotaBadge < BadgeBase
-  end
-
-  it 'should award user with badge' do
-    user = Fabricate :user
-    user.award(NotaBadge.new(user))
-    expect(user.badges.size).to eq(1)
-    expect(user.badges.first.badge_class_name).to eq(NotaBadge.name)
-  end
-
-  it 'instantiates new user with omniauth if the user is not on file' do
-    omniauth = { 'info' => { 'name' => 'Matthew Deiters', 'urls' => { 'Blog' => 'http://www.theagiledeveloper.com', 'GitHub' => 'http://github.com/mdeiters' }, 'nickname' => 'mdeiters', 'email' => '' }, 'uid' => 7330, 'credentials' => { 'token' => 'f0f6946eb12c4156a7a567fd73aebe4d3cdde4c8' }, 'extra' => { 'user_hash' => { 'plan' => { 'name' => 'micro', 'collaborators' => 1, 'space' => 614_400, 'private_repos' => 5 }, 'gravatar_id' => 'aacb7c97f7452b3ff11f67151469e3b0', 'company' => nil, 'name' => 'Matthew Deiters', 'created_at' => '2008/04/14 15:53:10 -0700', 'location' => '', 'disk_usage' => 288_049, 'collaborators' => 0, 'public_repo_count' => 18, 'public_gist_count' => 31, 'blog' => 'http://www.theagiledeveloper.com', 'following_count' => 27, 'id' => 7330, 'owned_private_repo_count' => 2, 'private_gist_count' => 2, 'type' => 'User', 'permission' => nil, 'total_private_repo_count' => 2, 'followers_count' => 19, 'login' => 'mdeiters', 'email' => '' } }, 'provider' => 'github' }
-
-    user = User.for_omniauth(omniauth.with_indifferent_access)
-    expect(user).to be_new_record
-  end
-
-  it 'increments the badge count when you add new badges' do
-    user = Fabricate :user
-
-    user.award(NotaBadge.new(user))
-    user.save!
-    user.reload
-    expect(user.badges_count).to eq(1)
-
-    user.award(AlsoNotaBadge.new(user))
-    user.save!
-    user.reload
-    expect(user.badges_count).to eq(2)
-  end
-
-  it 'should randomly select the user with badges' do
-    user = Fabricate :user
-    user.award(NotaBadge.new(user))
-    user.award(NotaBadge.new(user))
-    user.save!
-
-    user2 = Fabricate :user, username: 'different', github_token: 'unique'
-
-    4.times do
-      expect(User.random).not_to eq(user2)
+      expect(user.badges.first).to eq(badge3)
+      expect(user.badges.last).to eq(badge1)
     end
-  end
 
-  it 'should not allow adding the same badge twice' do
-    user = Fabricate :user
-    user.award(NotaBadge.new(user))
-    user.award(NotaBadge.new(user))
-    user.save!
-    expect(user.badges.count).to eq(1)
+    class NotaBadge < BadgeBase
+    end
+
+    class AlsoNotaBadge < BadgeBase
+    end
+
+    it 'should award user with badge' do
+      user = Fabricate :user
+      user.award(NotaBadge.new(user))
+      expect(user.badges.size).to eq(1)
+      expect(user.badges.first.badge_class_name).to eq(NotaBadge.name)
+    end
+
+    it 'should not allow adding the same badge twice' do
+      user = Fabricate :user
+      user.award(NotaBadge.new(user))
+      user.award(NotaBadge.new(user))
+      user.save!
+      expect(user.badges.count).to eq(1)
+    end
+
+    it 'increments the badge count when you add new badges' do
+      user = Fabricate :user
+
+      user.award(NotaBadge.new(user))
+      user.save!
+      user.reload
+      expect(user.badges_count).to eq(1)
+
+      user.award(AlsoNotaBadge.new(user))
+      user.save!
+      user.reload
+      expect(user.badges_count).to eq(2)
+    end
+
+    it 'should randomly select the user with badges' do
+      user = Fabricate :user
+      user.award(NotaBadge.new(user))
+      user.award(NotaBadge.new(user))
+      user.save!
+
+      user2 = Fabricate :user, username: 'different', github_token: 'unique'
+
+      4.times do
+        expect(User.random).not_to eq(user2)
+      end
+    end
   end
 
   describe 'redemptions' do
@@ -274,22 +290,6 @@ RSpec.describe User, type: :model do
       user = Fabricate.build(:user, redemptions: %w(foo))
       user.update_attributes redemptions: []
       expect(user.reload.redemptions).to be_empty
-    end
-  end
-
-  describe 'validation' do
-    it 'should not allow a username in the reserved list' do
-      User::RESERVED.each do |reserved|
-        user = Fabricate.build(:user, username: reserved)
-        expect(user).not_to be_valid
-        expect(user.errors[:username]).to eq(['is reserved'])
-      end
-    end
-
-    it 'should not allow a username with a period character' do
-      user = Fabricate.build(:user, username: 'foo.bar')
-      expect(user).not_to be_valid
-      expect(user.errors[:username]).to eq(['must not contain a period'])
     end
   end
 
@@ -325,48 +325,41 @@ RSpec.describe User, type: :model do
   describe '#team' do
     let(:team) { Fabricate(:team) }
     let(:user) { Fabricate(:user) }
+
     it 'returns membership team if user has membership' do
       team.add_member(user)
       expect(user.team).to eq(team)
     end
+
     it 'returns team if team_id is set' do
       user.team_id = team.id
       user.save
       expect(user.team).to eq(team)
     end
+
     it 'returns nil if no team_id or membership' do
       expect(user.team).to eq(nil)
     end
-  end
 
-  it 'should indicate when user is on a premium team' do
-    team = Fabricate(:team, premium: true)
-    member = team.add_member(user = Fabricate(:user))
-
-    expect(user.on_premium_team?).to eq(true)
-  end
-
-  it 'should indicate a user not on a premium team when they dont belong to a team at all' do
-    user = Fabricate(:user)
-    expect(user.on_premium_team?).to eq(false)
-  end
-
-  it 'should not error if the users team has been deleted' do
-    team = Fabricate(:team)
-    user = Fabricate(:user)
-    team.add_member(user)
-    team.destroy
-    expect(user.team).to be_nil
+    it 'should not error if the users team has been deleted' do
+      team = Fabricate(:team)
+      user = Fabricate(:user)
+      team.add_member(user)
+      team.destroy
+      expect(user.team).to be_nil
+    end
   end
 
   describe '#on_team?' do
     let(:team) { Fabricate(:team) }
     let(:user) { Fabricate(:user) }
+
     it 'is true if user has a membership' do
       expect(user.on_team?).to eq(false)
       team.add_member(user)
       expect(user.reload.on_team?).to eq(true)
     end
+
     it 'is true if user is on a team' do
       expect(user.on_team?).to eq(false)
       user.team = team
@@ -375,24 +368,48 @@ RSpec.describe User, type: :model do
     end
   end
 
-  it 'can follow another user' do
-    user = Fabricate(:user)
-    other_user = Fabricate(:user)
-    user.follow(other_user)
-    expect(other_user.followed_by?(user)).to eq(true)
+  describe "#on_premium_team?" do
+    it 'should indicate when user is on a premium team' do
+      team = Fabricate(:team, premium: true)
+      member = team.add_member(user = Fabricate(:user))
+      expect(user.on_premium_team?).to eq(true)
+    end
 
-    expect(user.following?(other_user)).to eq(true)
+    it 'should indicate a user not on a premium team when they dont belong to a team at all' do
+      user = Fabricate(:user)
+      expect(user.on_premium_team?).to eq(false)
+    end
   end
 
-  it 'should pull twitter follow list and follow any users on our system' do
-    expect(Twitter).to receive(:friend_ids).with(6_271_932).and_return(%w(1111 2222))
+  describe 'following' do
+    let(:user) { Fabricate(:user) }
+    let(:other_user) { Fabricate(:user) }
 
-    user = Fabricate(:user, twitter_id: 6_271_932)
-    other_user = Fabricate(:user, twitter_id: '1111')
-    expect(user).not_to be_following(other_user)
-    user.build_follow_list!
+    it 'can follow another user' do
+      user.follow(other_user)
 
-    expect(user).to be_following(other_user)
+      expect(other_user.followed_by?(user)).to eq(true)
+      expect(user.following?(other_user)).to eq(true)
+    end
+
+    it 'should pull twitter follow list and follow any users on our system' do
+      expect(Twitter).to receive(:friend_ids).with(6_271_932).and_return(%w(1111 2222))
+
+      user = Fabricate(:user, twitter_id: 6_271_932)
+      other_user = Fabricate(:user, twitter_id: '1111')
+      expect(user).not_to be_following(other_user)
+      user.build_follow_list!
+
+      expect(user).to be_following(other_user)
+    end
+
+    it 'should follow another user only once' do
+      expect(user.following_by_type(User.name).size).to eq(0)
+      2.times do
+        user.follow(other_user)
+        expect(user.following_by_type(User.name).size).to eq(1)
+      end
+    end
   end
 
   describe 'skills' do
@@ -444,19 +461,6 @@ RSpec.describe User, type: :model do
     it 'indicates when a user has seen a feature' do
       user.seen('some_feature')
       expect(user.seen?('some_feature')).to eq(true)
-    end
-  end
-
-  describe 'following' do
-    let(:user) { Fabricate(:user) }
-    let(:followable) { Fabricate(:user) }
-
-    it 'should follow another user only once' do
-      expect(user.following_by_type(User.name).size).to eq(0)
-      user.follow(followable)
-      expect(user.following_by_type(User.name).size).to eq(1)
-      user.follow(followable)
-      expect(user.following_by_type(User.name).size).to eq(1)
     end
   end
 
--- spec/models/users/github/profile_spec.rb
+++ spec/models/users/github/profile_spec.rb
@@ -22,12 +22,12 @@
 require 'rails_helper'
 require 'vcr_helper'
 
-RSpec.describe Users::Github::Profile, type: :model, skip: true do
+RSpec.describe Users::Github::Profile, type: :model do
   it { is_expected.to belong_to :user }
   it { is_expected.to have_many :followers }
   it { is_expected.to have_many :repositories }
 
-  context 'creation',  vcr: { cassette_name: 'github_for seuros', record: :new_episodes } do
+  context 'creation',  vcr: { cassette_name: 'github_for seuros'} do
     it 'should get info from github' do
       user = Fabricate(:user) { github 'seuros' }
       profile = user.create_github_profile
@@ -35,7 +35,6 @@ RSpec.describe Users::Github::Profile, type: :model, skip: true do
 
       expect(profile.name).to eq('Abdelkader Boudih')
       expect(profile.github_id).to eq(2_394_703)
-
     end
   end
 end
