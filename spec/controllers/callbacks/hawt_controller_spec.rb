56f7ffdc3ec47b73f816f2ceef992ce9f5d2cc5b
Abdelkader Boudih <terminale@gmail.com>
2015-03-03 16:08:48 +0000

    fix spec 2

--- spec/controllers/callbacks/hawt_controller_spec.rb
+++ spec/controllers/callbacks/hawt_controller_spec.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-require File.expand_path('../../../app/services/protips/hawt_service')
+require File.expand_path('services/protips/hawt_service')
 
 RSpec.describe Callbacks::HawtController, type: :controller do
   include AuthHelper
