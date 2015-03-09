29e3239d07c72142aedef05fe041f0af0007bf58
Abdelkader Boudih <terminale@gmail.com>
2015-03-09 07:22:39 +0000

    change sidekiq database

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,21 @@
 # https://devcenter.heroku.com/articles/forked-pg-connections#sidekiq
+redis_url = (ENV[ENV['REDIS_PROVIDER'] || 'REDIS_URL'])
+sidekiq_redis_url = redis_url + '/2' # Use third database
+
 Sidekiq.configure_server do |config|
   database_url = ENV['DATABASE_URL']
   if database_url
     ENV['DATABASE_URL'] = "#{database_url}?pool=25"
     ActiveRecord::Base.establish_connection
+  end
+  if redis_url
+    config.redis = { url: sidekiq_redis_url }
+  end
+end
+
+Sidekiq.configure_client do |config|
+  if redis_url
+    config.redis = { url: sidekiq_redis_url }
   end
 end
 
