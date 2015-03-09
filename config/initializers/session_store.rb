4881417644eb51834e6dd8f1feb4f634fa6e72ac
Abdelkader Boudih <terminale@gmail.com>
2015-03-09 06:55:06 +0000

    change session_store database

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,2 @@
 # Be sure to restart your server when you modify this file.
-Coderwall::Application.config.session_store :redis_store
+Rails.application.config.session_store :redis_store, {:db => 1, :namespace => 'cache'}
