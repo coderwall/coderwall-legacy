b0bec593bc79d0da14a20f1afeb64ed26b2fd98d
Abdelkader Boudih <terminale@gmail.com>
2015-03-13 16:27:13 +0000

    refactoring

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    return redirect_to destination_url, flash: flash if signed_in?
+    redirect_to destination_url, flash: flash if signed_in?
   end
 end
