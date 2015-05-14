5a04e3529ce857f9f018fed7e544d4a2f29fa3b6
Abdelkader Boudih <terminale@gmail.com>
2015-05-14 09:40:13 +0100

    Fix comment bug + cleanup

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,9 @@
 class CommentsController < ApplicationController
 
-  before_action :access_required, only: [:new, :edit, :update, :destroy]
-  before_action :verify_ownership, only: [:edit, :update, :destroy]
-  before_action :require_admin!, only: [:flag, :index]
+  before_action :access_required, only: [:update, :destroy]
   before_action :lookup_comment, only: [:edit, :update, :destroy, :like]
+  before_action :verify_ownership, only: [:edit, :update, :destroy]
   before_action :lookup_protip, only: [:create]
-
-  def index
-    @comments = Comment.where('created_at > ?', 1.day.ago)
-  end
-
-  def new ; end
-
-  def edit ; end
 
   def create
     create_comment_params = params.require(:comment).permit(:comment)
@@ -25,10 +16,10 @@ class CommentsController < ApplicationController
 
       if @comment.save
         record_event('created comment')
-        format.html { redirect_to protip_path(@comment.commentable) }
+        format.html { redirect_to protip_path(params[:protip_id]) }
         format.json { render json: @comment, status: :created, location: @comment }
       else
-        format.html { redirect_to protip_path(@comment.commentable), error: "could not add your comment. try again" }
+        format.html { redirect_to protip_path(params[:protip_id]), error: "could not add your comment. try again" }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
@@ -39,10 +30,10 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.update_attributes(update_comment_params)
-        format.html { redirect_to protip_path(@comment.commentable.try(:public_id)) }
+        format.html { redirect_to protip_path(params[:protip_id]) }
         format.json { head :ok }
       else
-        format.html { redirect_to protip_path(@comment.commentable.try(:public_id)), error: "could not update your comment. try again" }
+        format.html { redirect_to protip_path(params[:protip_id]), error: "could not update your comment. try again" }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
@@ -70,18 +61,15 @@ class CommentsController < ApplicationController
   private
 
   def lookup_comment
-    id = params.permit(:id)[:id]
-    @comment = Comment.find(id)
+    @comment = Comment.find(params[:id])
     lookup_protip
   end
 
   def lookup_protip
-    protip_id = params.permit(:protip_id)[:protip_id]
-    @protip = Protip.with_public_id(protip_id)
+    @protip = Protip.find_by_public_id(params[:protip_id])
   end
 
   def verify_ownership
-    lookup_comment
     redirect_to(root_url) unless (is_admin? or (@comment && @comment.authored_by?(current_user)))
   end
 end
