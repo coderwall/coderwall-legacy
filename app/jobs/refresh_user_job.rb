7b95159c423b95808989feba443af65c554e9f71
Abdelkader Boudih <terminale@gmail.com>
2015-05-21 22:37:20 +0100

    exit if user was deleted

--- app/jobs/refresh_user_job.rb
+++ app/jobs/refresh_user_job.rb
@@ -4,19 +4,22 @@ class RefreshUserJob
 
   def perform(user_id, full=false)
     return if Rails.env.test?
-
-    user = User.find(user_id)
-
-    return if !full && user.last_refresh_at > 3.days.ago
-
     begin
-      user.build_facts(full)
-      user.reload.check_achievements!
-      user.add_skills_for_unbadgified_facts
+      user = User.find(user_id)
 
-      user.calculate_score!
-    ensure
-      user.touch(:last_refresh_at)
+      return if !full && user.last_refresh_at > 3.days.ago
+
+      begin
+        user.build_facts(full)
+        user.reload.check_achievements!
+        user.add_skills_for_unbadgified_facts
+
+        user.calculate_score!
+      ensure
+        user.touch(:last_refresh_at)
+      end
+    rescue ActiveRecord::RecordNotFound
+      return
     end
   end
 end
--- app/workers/user_activate_worker.rb
+++ app/workers/user_activate_worker.rb
@@ -3,24 +3,27 @@ class UserActivateWorker
   sidekiq_options queue: :user
 
   def perform(user_id)
-    user = User.find(user_id)
-    return if user.active?
-
     begin
-      NotifierMailer.welcome_email(user.id).deliver
-      RefreshUserJob.new.perform(user.id)
-      user.activate!
+      user = User.find(user_id)
+      return if user.active?
 
-    rescue => e
-      #User provided corrupted email, we can't email.
-      if e.message == '550 5.1.3 Invalid address'
-        user.destroy
-        return
-      else
-        raise e
+      begin
+        NotifierMailer.welcome_email(user.id).deliver
+        RefreshUserJob.new.perform(user.id)
+        user.activate!
+
+      rescue => e
+        #User provided corrupted email, we can't email.
+        if e.message == '550 5.1.3 Invalid address'
+          user.destroy
+          return
+        else
+          raise e
+        end
+
       end
-
+    rescue ActiveRecord::RecordNotFound
+      return
     end
-
   end
 end
