d019ae0e0d27da3c45b336ea02d5f07cfc033d18
Abdelkader Boudih <terminale@gmail.com>
2015-05-22 00:06:56 +0100

    fix tests

--- spec/workers/user_activate_worker_spec.rb
+++ spec/workers/user_activate_worker_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe UserActivateWorker do
     context 'when invalid user' do
       let(:user_id) { 1 }
 
-      it { expect { worker.perform(user_id) }.to_not raise_error ActiveRecord::RecordNotFound }
+      it { expect { worker.perform(user_id) }.not_to raise_error }
     end
 
     context 'when pending user' do
