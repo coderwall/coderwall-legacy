f9d6776de7ccbe4c864da1ecf65caeda9a717d12
Abdelkader Boudih <terminale@gmail.com>
2015-05-31 13:15:20 +0100

    remove protip importer job

--- spec/jobs/import_protip_job_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe ImportProtipJob do
-
-  describe 'queueing' do
-    it 'pushes jobs to the correct queue' do
-      expect(ImportProtipJob.get_sidekiq_options['queue']).to eql :protip
-    end
-  end
-
-end
