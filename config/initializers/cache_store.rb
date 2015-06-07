aca284b836e8e7c43ebd4e6228e9d668fb2be72e
Abdelkader Boudih <terminale@gmail.com>
2015-06-07 18:09:59 +0100

    change redis databases

--- config/initializers/cache_store.rb
+++ config/initializers/cache_store.rb
@@ -1,3 +1,3 @@
 Coderwall::Application.configure do
-  config.cache_store = :redis_store, "#{ENV[ENV['REDIS_PROVIDER'] || 'REDIS_URL']}/#{ENV['REDIS_CACHE_STORE'] || 2}"
+  config.cache_store = :redis_store, "#{ENV[ENV['REDIS_PROVIDER'] || 'REDIS_URL']}/#{ENV['REDIS_CACHE_STORE'] || 3}"
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,2 @@
 # Be sure to restart your server when you modify this file.
-Rails.application.config.session_store :redis_store, {:db => 1, :namespace => 'cache'}
+Rails.application.config.session_store :redis_store, {:db => 4, :namespace => 'cache'}
