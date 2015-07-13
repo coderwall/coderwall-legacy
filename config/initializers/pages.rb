13f55fe594f827ea10176341103055edd46ac71b
Abdelkader Boudih <terminale@gmail.com>
2015-07-13 23:50:59 +0000

    fix page initializer

--- config/initializers/pages.rb
+++ config/initializers/pages.rb
@@ -1,15 +1,15 @@
-# Look at the *.html.haml files in the app/views/pages directory
-STATIC_PAGES ||= Dir.glob('app/views/pages/*.html.{erb,haml}')
-  .map { |f| File.basename(f, '.html.erb') }
-  .map { |f| File.basename(f, '.html.haml') }
+# Look at the *.html files in the app/views/pages directory
+STATIC_PAGES ||= Dir.glob('app/views/pages/*.html*')
+  .map { |f| File.basename(f, '.html.slim') }
+  .map { |f| File.basename(f, '.html') }
   .reject{ |f| f =~ /^_/ }
   .sort
   .uniq
 
-# Look at the *.html.haml files in the app/views/pages directory
-STATIC_PAGE_LAYOUTS ||= Dir.glob('app/views/layouts/*.html.{erb,haml}')
+# Look at the *.html files in the app/views/pages directory
+STATIC_PAGE_LAYOUTS ||= Dir.glob('app/views/layouts/*.html*')
   .map { |f| File.basename(f, '.html.erb') }
-  .map { |f| File.basename(f, '.html.haml') }
+  .map { |f| File.basename(f, '.html.slim') }
   .reject{ |f| f =~ /^_/ }
   .sort
   .uniq
