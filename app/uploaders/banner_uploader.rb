2c1774b687c361db2fe4abac0d7df9bdb32cd954
Abdelkader Boudih <terminale@gmail.com>
2015-08-23 16:59:21 +0100

    wip apply avatar if banner  is not upload

--- app/uploaders/banner_uploader.rb
+++ app/uploaders/banner_uploader.rb
@@ -7,4 +7,8 @@ class BannerUploader < CoderwallUploader
     system "convert #{tmpfile} -sigmoidal-contrast 7x50% \\( +clone -sparse-color Barycentric '0,0 black 0,%h white' -function polynomial 4.5,-4.5,1 \\) -compose Blur -set option:compose:args 15 -composite #{current_path}"
     File.delete(tmpfile)
   end
+
+  def default_url
+    model.avatar.url
+  end
 end
